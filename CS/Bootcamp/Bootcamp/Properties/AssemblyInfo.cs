3eb1d7b4cc90bf74360ff0f11467a447480286b1
Jasper de Boer <jasper.de.boer@diymaxeda.com>
2017-12-28 09:23:25 +0100

    Added setup for FirstSelenium test. Renamed helloWorld test. Uodated gitignore file

---
+++ CS/Bootcamp/Bootcamp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Bootcamp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bootcamp")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a8a6070d-22b9-4910-a8f5-2ddbdc65ff99")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CS/Bootcamp/Bootcamp/helloWorldTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bootcamp
+{
+    [TestClass]
+    public class HelloWorldTest
+    {
+        [TestMethod]
+        public void printText()
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
