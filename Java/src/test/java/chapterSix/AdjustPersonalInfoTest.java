1066169d1c90a2ecfc321e1199ae1f7cd0c5b1f5
Jasper de Boer <jasper.de.boer@diymaxeda.com>
2017-12-29 11:50:22 +0100

    dded documentation

--- Java/src/test/java/chapterSix/AdjustPersonalInfoTest.java
+++ Java/src/test/java/chapterSix/AdjustPersonalInfoTest.java
@@ -1,33 +1,30 @@
 package chapterSix;
 
+import org.assertj.core.api.AbstractBooleanAssert;
 import org.assertj.core.api.Assertions;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class AdjustPersonalInfoTest extends TestShopScenario {
 
-    public Boolean userLoggedIn() {
+    public AbstractBooleanAssert<?> userLoggedIn() {
         String logInButton = driver.findElement(By.className("login")).getText();
-        if (logInButton == "Sign in") {
-            return false;
-        }
-            else {
-               return true;
-           }
+        return Assertions.assertThat(logInButton.equals("Sign out"));
     }
 
-    public void createAccount() {
-        if (userLoggedIn = false) {
+    public void createAccount(Boolean userLoggedIn) {
+        if (userLoggedIn) {
             driver.findElement(By.className("login")).click();
             driver.findElement(By.id("email")).sendKeys("testjasper@polteq.com");
             driver.findElement(By.id("passwd")).sendKeys("Testje");
             driver.findElement(By.id("SubmitLogin")).click();
             driver.findElement(By.className("account")).click();
             driver.findElement(By.xpath("//ul[@class='myaccount-link-list']/li[5]/a/span")).click();
-            String firstName = driver.findElement(By.xpath("//input[@id=firstname][@value]));
-            if (firstName == "Jasper") {
+            WebElement firstName = driver.findElement(By.xpath("//input[@id='firstname']"));
+            if (firstName.getAttribute("value").equals("Jasper")) {
                 driver.findElement(By.className("first name")).clear();
                 driver.findElement(By.className("first name")).sendKeys("Naam2");
-            } else if (firstName == "Naam2") {
+            } else if (firstName.getAttribute("value").equals("Naam2")) {
                 driver.findElement(By.className("first name")).clear();
                 driver.findElement(By.className("first name")).sendKeys("Naamadres");
             } else {
